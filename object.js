07c01aa27780e5aa27a9ef45d3d18e5ea689fb48
Suraj Kumar Surywanshee <151986884+surajsurywanshee@users.noreply.github.com>
2024-06-07 16:40:52 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,91 @@
+// object in javascript
+
+
+let obj={}
+obj.name= ' Suraj '
+console.log(obj)
+obj.address= ' rerua khurd'
+obj.code= 485446
+obj.no= 6232573530
+
+
+// 1. create  operations
+
+let obj1 = {}
+console.log(obj1)
+
+let obj2= new Object()
+console.log(obj2)
+
+// key : 'djfhhsjerfjw'
+
+obj1.section = 'bia-4'
+obj1.center = 'ddygky'
+console.log(obj1)
+
+// 2. read operations
+
+let car={
+
+name :'thar',
+model :'4*4',
+engine : '1000cc',
+drivemode : 'auto',
+
+}
+
+console.log(car.name)
+
+ // insert operations
+
+let ddugky = {}
+
+ddugky.fundedBy ='DDUGKY'
+ddugky.runBy = 'JSMIES'
+ddugky.totalRunnigBatch = '4'
+ddugky.totalStudent = '60'
+
+console.log(ddugky)
+
+ //3. update operations
+
+let computer = {
+    father : 'charls',
+    created : 1914,
+    part : ['CPU', 'Monitor','Mouse'],
+    type : 3
+}
+
+console.log(computer)
+
+computer.type = 4
+computer.father = 'suraj'
+
+console.log(computer)
+
+//4. delete operations
+
+ computer = {
+    father : 'charls',
+    created : 1914,
+    part : ['CPU', 'Monitor','Mouse'],
+    type : 3
+}
+
+console.log(computer)
+
+delete computer.father
+delete computer.part
+console.log(computer)
+
+let suraj = {
+    name: ' suraj kumar surywanshee',
+    Section: 'bia4',
+    skills: ['java','javascript','sql'],
+    villege: 'gram rerua khurd post patwara district satna mp',
+    fathersDetail : {
+        name : 'Ramkaran surywanshee',
+        age : 63,
+        work : 'forming',
+    }
+}
---
+++ printnumber.js
@@ -0,0 +1,49 @@
+function print(num) {
+    for(let i=1; i<=num; i++)  {
+        let star=''
+        for(let j=1; j<=i*2-1; j++) {
+            star=star+"*"
+        }
+        console.log(star)
+    }
+}
+
+print(8)
+
+function ninjaNumber(n) {
+    let count=1
+    for(let i=1; i<=n; i++)  {
+        let str=' '
+        for(let j= 1; j<=(n-i);j++){
+            str= str + ' '
+        }
+
+    for(let k=1; k <= i; k++) {
+        str= str + count
+        count++
+    }
+
+    console.log(str)
+ }
+
+}
+
+ninjaNumber(8)
+
+
+function ninjaNumber(n) {
+    let count=1
+    for(let i=1; i<=n; i++)  {
+        let str=' '
+        for(let j= 1; j<=(n-i);j++){
+            str= str + ' '
+        }
+    for(let k=1; k <= i; k++) {
+        str= str + count
+        count++
+    }
+
+    console.log(str)
+ }
+}
+ninjaNumber(5)
