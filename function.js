d39c3fb6c8fcb97467aca87643ad709ebf0c0b4a
Suraj Kumar Surywanshee <151986884+surajsurywanshee@users.noreply.github.com>
2024-06-08 14:21:34 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,55 +1,53 @@
-//addition
+// 1.Addition
 
 function sum(a,b)
-{
+    {
     return a+b
-}
+    }
 console.log(sum(59,38))
 
-//subtract
+// 2.Subtract
 
 function sub(a,b)
-{
+    {
     return a-b
-}
-
+    }
 console.log(sub(34,69))
 
-//multiply
+// 3.Multiply
 
 function multiply(a,b)
-{
+    {
     return a*b
-}
-
+    }
 console.log(multiply(34,69))
 
-//divide
+// 4.Divide
 
 function divide(a,b)
-{
+    {
     return a/b
-}
-
+    }
 console.log(divide(34,69))
 
-// Voter eligibility
+// 5.Voter Eligibility
 
 console.log('hello')
 function voter(age)
 {
     if(age >= 18)
-    {
+        {
         console.log('voter eligible the giving a vote')
-    }
+        }
     else
-    {
+        {
         console.log('voter not eligible to giving a vote')
-    }
+        }
 }
-
 voter(46)
-// Average Number
+
+// 6.Average Number
+
 function averageGrade(arr) {
     let sum=0;
   for(let i=0; i< arr.length; i++)
@@ -61,79 +59,70 @@ function averageGrade(arr) {
     console.log(avr)
     return avr
 }
-
     averageGrade([4,35,76,76,34,69])
 
-// Oddsum Average
+// 7.Oddsum Average
+
 function oddball_sum(nums){
        let sum =0;
        let oddsum = 0;
        for(let i=0; i<nums.length; i++)
         {
             if(nums[i] %2==1)
-            {
+                {
                 oddsum = oddsum+nums[i]
-            }
-
+                }
         }
             console.log(oddsum)
-        }
+ }
         let oddballArray= [3,55,7,37,38,58,49,765,7]
-
         oddball_sum(oddballArray)
 
-// Array Length
+// 8.Array Length
 
 function getArray(arg)
-{
+    {
     console.log(arg.length)
-}
-
+ }
 getArray([2,4,3,5,2,5,32,5,3])
 
-
-// Celsius to Fahrenheit
+// 9.Celsius to Fahrenheit
 
 function celsius(c)
- {
+     {
    let fahrenheit = (c*1.8) +32
    return fahrenheit
 }
-
 console.log(celsius(34))
 
-// kilometer to meter
+// 10.Kilometer to Meter
 
 function kmTom(km)
-{
+    {
     let meter=(km*1000)
     return meter
 }
-
 console.log(kmTom(45))
 
-
-// dolor to rupee
+// 11.Dolor to Rupee
 
 function dTor(dolor)
-{
+    {
     let rupee=(dolor*83.30)
     return rupee
 }
-
 console.log(dTor(45))
 
-// fut to cm
+// 12.Fut to Cm
 
 function futToCm(fut)
-{
+    {
     let centimeter=(fut*12)
     return centimeter
 }
-
 console.log(futToCm(45))
 
-//4. Sum And Avg Values
+// 13.Sum And Avg Values
 
 let dolphinScore = [96,88,89]
 let koalasScore = [88,96,89]
@@ -144,23 +133,19 @@ for(let i=0; i< dolphinLength; i++) {
     let value = dolphinScore[i]
     sum = sum + value
 }
-
     console.log(sum)
     let dolphinAvr = sum/dolphinLength
     console.log(dolphinAvr)
 
 let koalasLength = koalasScore.length
-
     sum=0
 for(let i=0; i< koalasLength; i++) {
     let value = koalasScore[i]
     sum = sum + value
 }
-
     console.log(sum)
     let koalasAvr = sum/ koalasLength
     console.log(koalasAvr)
-
 
     if(dolphinAvr > koalasAvr && dolphinAvr>100) {
         console.log('dolphin win match')
