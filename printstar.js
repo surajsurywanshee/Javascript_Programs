7bc9963672ca29331cb0b628df4deff876ebe0f1
Suraj Kumar Surywanshee <151986884+surajsurywanshee@users.noreply.github.com>
2024-06-08 14:35:52 +0530

    Update printstar.js

--- printstar.js
+++ printstar.js
@@ -180,3 +180,49 @@ function print(num) {
         }
     }
 print(8)
+
+// 14.
+function print(num) {
+    for(let i=1; i<=num; i++)  {
+        let star=''
+        for(let j=1; j<=i*2-1; j++) {
+            star=star+"*"
+        }
+        console.log(star)
+    }
+}
+print(8)
+
+// 15.
+function ninjaNumber(n) {
+    let count=1
+    for(let i=1; i<=n; i++)  {
+        let str=' '
+        for(let j= 1; j<=(n-i);j++){
+            str= str + ' '
+        }
+    for(let k=1; k <= i; k++) {
+        str= str + count
+        count++
+    }
+    console.log(str)
+ }
+}
+ninjaNumber(8)
+
+// 16.
+function ninjaNumber(n) {
+    let count=1
+    for(let i=1; i<=n; i++)  {
+        let str=' '
+        for(let j= 1; j<=(n-i);j++){
+            str= str + ' '
+        }
+    for(let k=1; k <= i; k++) {
+        str= str + count
+        count++
+    }
+    console.log(str)
+ }
+}
+ninjaNumber(5)
