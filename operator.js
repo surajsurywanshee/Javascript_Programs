ef2b647f73993454346a51cc836a576606a3ac5b
Suraj Kumar Surywanshee <151986884+surajsurywanshee@users.noreply.github.com>
2024-06-08 14:08:24 +0530

    Update operator.js

---
+++ operator.js
@@ -0,0 +1,104 @@
+//1. Arithamatic Oprators
+
+ concadination only apply on + operator it cant use - operatior
+
+console.log(2+4)
+console.log(10-4)
+console.log(5*6)
+console.log(47/6)
+console.log(5%3)
+console.log(3**10)
+
+console.log('45'+'34')
+console.log(2+'2')
+console.log('4'+6)
+
+console.log(5+8+'6')
+
+console.log(19 -'2')
+console.log(typeof(10-'2'))
+
+console.log('10'-6)
+console.log(typeof('10'-6))
+
+console.log('suraj'-6)
+console.log(typeof('suraj'-6))
+
+console.log(3+"6"-5)
+
+console.log(7*6)
+console.log(4*"5")
+
+console.log('5'*'5')
+console.log(typeof('5'*'5'))
+
+console.log('suraj'*'5')
+console.log(typeof('suraj'*'5'))
+
+console.log(45/3)
+console.log('45'/3)
+console.log('45'/'3')
+console.log(typeof(5/'3'))
+
+console.log(45%6)
+console.log("53434"%3)
+console.log('457565'%'53')
+
+console.log(typeof('457565'%'53'))
+console.log(typeof("Suraj Pratima" %'53'))
+
+console.log('10'**3)
+
+let n= 45
+n++
+ console.log(n++)
+ --n
+ ++n
+ console.log(n--)
+ console.log(n)
+
+
+ let n='23'
+ n--
+ --n
+ console.log(n)
+
+ n++
+ ++n
+ console.log(n)
+
+let n=20
+n++
+console.log(n+++n)
+
+
+Comparison Operators
+
+console.log(2>5)
+console.log(3>5)
+console.log(5>=5)
+console.log(3<=5)
+
+console.log(5<9)
+console.log(3<5)
+
+console.log('22'>21)
+console.log('22'>='22')
+
+console.log(47<=50)
+console.log(55==50)
+console.log('47'==47)
+
+console.log(2!=2)
+console.log('2'!=2)
+
+console.log(3=='3')
+console.log(50==='50')
+console.log('3'==='3')
+
+console.log('a'>'b')
+console.log('abc'>'bca')
+console.log('shubham'>'shivam')
+console.log('suraj'>'pratima')
+
+console.log('Z'>'y')
